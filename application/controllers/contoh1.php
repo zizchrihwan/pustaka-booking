27b796ef5f47cec84a80185dd06292cfbb29ff81
ramadhankhaizan7375@gmail.com <ramadhankhaizan7375@gmail.com>
2021-10-07 18:59:52 +0700

    Upload awal project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Dwi Muhammad Ramadhan Saya tinggal di daerah Cileungsi olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
---
+++ application/views/view-Latihan1.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    <h1>Hallo Kawan... Yuk kita belajar Web Programming..!!!<h1><br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 ." = " . $hasil; ?>
+</body>
+
+</html>
