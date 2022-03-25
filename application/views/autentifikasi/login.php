ce632e0214218214fe504b9aa672e3f8e00cb191
ramadhankhaizan7375@gmail.com <ramadhankhaizan7375@gmail.com>
2022-03-25 16:15:25 +0700

    wp 2

--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -24,7 +24,7 @@
                                         <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password">
                                         <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
-                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                    <button type="submit" class="btn btn-info btn-user btn-block">
                                         Masuk
                                     </button>
                                 </form>
